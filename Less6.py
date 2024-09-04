90ff74554c3b74e5d902eb5499d34892fb05a100
TonyCleem <tonycleem@gmail.com>
2024-09-04 21:05:08 +0700

    Update Less6.py

--- Less6.py
+++ Less6.py
@@ -2,33 +2,27 @@ import urwid
 
 
 def digit_check(password, score):
-    score+=2 * any(digit.isdigit() for digit in password)
-    return score
-
+    return any(digit.isdigit() for digit in password)
     
+
 def letters_check(password, score):
-    score+=2 * any(word.isalpha()  for word in password)
-    return score
+    return any(word.isalpha()  for word in password)
 
     
 def length_check(password, score):
-    score+=2 * (len(password) > 12)
-    return score
-
+    return (len(password) > 12)
+    
 
 def lower_letters_check(password, score):
-    score+=2 * any(word.islower() for word in password)
-    return score
+    return any(word.islower() for word in password)
 
     
 def upper_letters_check(password, score):
-    score+=2 * any(word.isupper() for word in password)
-    return score
+    return any(word.isupper() for word in password)
     
     
 def symbols_check(password, score):
-    score+=2 * any(not(sym.isalpha() or sym.isdigit() or '') for sym in password)
-    return score
+    return any(not(sym.isalpha() or sym.isdigit() or '') for sym in password)
     
     
 def on_ask_change(edit, new_edit_text):
@@ -36,7 +30,7 @@ def on_ask_change(edit, new_edit_text):
     check = [upper_letters_check, length_check, digit_check, symbols_check, letters_check, lower_letters_check]
     password = new_edit_text
     for c in check:
-        score = c(password, score)
+        score+=2 * c(password, score)
     reply.set_text("Рейтинг пароля: " + str(score))
         
 
