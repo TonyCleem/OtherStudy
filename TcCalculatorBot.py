d6330913ba1059a2a69112ed1f3e5f719acfa544
TonyCleem <tonycleem@gmail.com>
2024-09-08 17:54:31 +0700

    new commit

--- TcCalculatorBot.py
+++ TcCalculatorBot.py
@@ -8,21 +8,36 @@ TG_TOKEN = os.getenv('TG_TOKEN')
 TG_CHAT_ID = os.getenv('TG_CHAT_ID')
 
 
+def wait(chat_id, question):
+    message_id = bot.send_message(chat_id,"Запускаю таймер")
+    bot.create_countdown(
+        parse(question),
+        notify_progress,
+        val_bar=parse(question),
+        message_id=message_id,
+        forward_id=chat_id,
+        )
+    bot.create_timer(
+        parse(question),
+        choose,
+        forward_id=chat_id,
+        forward_answer=question,
+        )
+
+
 def notify_progress(secs_left, message_id, forward_id, val_bar):
     val_progressbar = render_progressbar(val_bar, val_bar - secs_left)
     new_message = "Осталось {} сек\n".format(secs_left) + val_progressbar
-    bot.update_message(forward_id, message_id, new_message)
+    bot.update_message(
+        forward_id,
+        message_id,
+        new_message,
+        )
 
 
 def choose(forward_id, forward_answer):
     final_message = "Время вышло!"
     bot.send_message(forward_id, final_message)
-
-
-def wait(chat_id, question):
-    message_id = bot.send_message(chat_id, "Запускаю таймер")
-    bot.create_countdown(parse(question), notify_progress, val_bar=parse(question), message_id=message_id, forward_id=chat_id)
-    bot.create_timer(parse(question), choose, forward_id=chat_id, forward_answer=question)
 
 
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
