96bcbc0054cd3b55aaef5a3436f92bc9791df808
TonyCleem <tonycleem@gmail.com>
2024-09-25 20:42:58 +0700

    corrected the comments

--- get_weather/get_weather.py
+++ get_weather/get_weather.py
@@ -1,24 +1,28 @@
 import requests
 
 
-def get(
-        article_id, option,
-        language,
-    ):
-    url_template = 'https://wttr.in/{}?{}&{}'
-    url = url_template.format(article_id, option, language)
-    response = requests.get(url)
+def get_weather_forecast(
+                         url,
+                         weather,
+                         payload,
+                        ):
+    response = requests.get(url, params=payload)
     response.raise_for_status()
-    result = response.text
-    return print(result)
+    weather = response.text
+    return weather
 
 
 def main():
-    option = 'nqmMT'
-    language = 'lang=ru'
-    article_id = ['Шереметьево', 'Лондон', 'Череповец']
-    for point in article_id:
-        get(point, option, language)
+    weather = ''
+    payload = {
+               'nqmMT': '',
+               'lang=ru': '',
+               }
+    url_template = 'https://wttr.in/{}'
+    location_search = ['Шереметьево', 'Лондон', 'Череповец']
+    for location in location_search:
+        url = url_template.format(location)
+        print(get_weather_forecast(url, weather, payload))
 
 
 if __name__ == "__main__":
