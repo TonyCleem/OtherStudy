1a001f16024ca1bf1e11e24868978f3d98d57520
TonyCleem <tonycleem@gmail.com>
2024-10-05 19:43:28 +0700

    Fix vk code

--- vk_app/vk.py
+++ vk_app/vk.py
@@ -1,23 +1,34 @@
 import requests
 import os
-import pprint
 from dotenv import load_dotenv
 from urllib.parse import urlparse
 
 
+def is_shorten_link(url):
+    is_shorten_link = urlparse(url)
+    if is_shorten_link.netloc == 'vk.cc':
+        return is_shorten_link.path[1:]
+    else:
+        return False
 
 
-def test_user_link(user_input):
-    test_user_link = requests.get(user_input)
-    test_user_link.raise_for_status()
-    test_user_link = test_user_link.ok
-    return test_user_link
+def test_user_input(user_input):
+    try:
+        test_user_input = requests.get(user_input)
+        test_user_input.raise_for_status()
+        return True
+    except requests.exceptions.HTTPError:
+        return False
+    except requests.exceptions.SSLError:
+        return False
+    except Exception:
+        return False
 
 
-def shorten_link(token, link):
+def shorten_link(token, user_input):
     params = {
         'access_token': token,
-        'url': link,
+        'url': user_input,
         'v': '5.131',
     }
     url = 'https://api.vk.com/method/utils.getShortLink'
@@ -25,49 +36,38 @@ def shorten_link(token, link):
     shorten_link.raise_for_status()
     shorten_link = shorten_link.json()
     shorten_link = shorten_link['response']['short_url']
-
-    url = '{key}'
-    url = url.format(key=shorten_link)
-    parsed = urlparse(url)
+    parsed = urlparse(shorten_link)
     parsed = parsed.path[1:]
     return parsed
 
-def count_clicks(token, link, key, interval):
+
+def count_clicks(token, key):
     params = {
         'access_token': token,
-        'url': link,
         'key': key,
-        'interval': interval,
+        'interval': 'forever',
         'v': '5.131',
     }
     url = 'https://api.vk.com/method/utils.getLinkStats'
-
     count_clicks = requests.get(url, params=params)
     count_clicks.raise_for_status()
     count_clicks = count_clicks.json()
+    count_clicks = count_clicks['response']['stats'][0]['views']
     return count_clicks
-
 
 
 if __name__ == '__main__':
     load_dotenv()
     token = os.getenv('API_TOKEN')
-    method = 'utils.getShortLink'
-    url = 'https://api.vk.com/method/{method}'
     user_input = input('Enter your URL: ')
-
-    try:
-        test_user_link(user_input)
-        if test_user_link:
+    if test_user_input(user_input):
+        if is_shorten_link(user_input):
+            is_shorten_link = is_shorten_link(user_input)
+            count_clicks = count_clicks(token, is_shorten_link)
+            print('Count clicks: ', count_clicks)
+        else:
             shorten_link = shorten_link(token, user_input)
-            interval_input = input('Enter interval (hour, day, week, month, forever): ')
-            count_clicks = count_clicks(token, user_input, shorten_link, interval_input)
-            pprint.pprint(count_clicks)
-    except requests.exceptions.HTTPError:
+            count_clicks = count_clicks(token, shorten_link)
+            print('Count clicks: ', count_clicks)
+    else:
         print('Invalid URL')
-
-
-
-
-#      https://dvmn.org/modules
-#                                 https://dvmns.org/modules
